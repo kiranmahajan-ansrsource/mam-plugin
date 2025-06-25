ab4366c9f9c0ad707fc24f08640684d840603d9a
Kiran Mahajan <kiran.mahajan@ansrsource.com>
2025-06-25 18:00:10 +0530

    feat: add security params and deeplinking integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const lti = require("ltijs").Provider;
+const { DeepLinkingService } = require("ltijs");
 
 lti.setup(
   process.env.LTI_KEY,
@@ -11,22 +12,43 @@ lti.setup(
   {
     appRoute: "/",
     cookies: {
-      secure: false,
-      sameSite: "",
+      secure: true,
+      sameSite: "None",
     },
     devMode: false,
   }
 );
 
 lti.onConnect((token, req, res) => {
-  console.log(token);
-  return res.redirect("/");
+  console.log("LTI Launch successful", token);
+  return lti.redirect(res, "/");
+});
+
+lti.onDeepLinking(async (token, req, res) => {
+  console.log("Deep Linking initiated", token);
+
+  const items = [
+    {
+      type: "ltiResourceLink",
+      title: "MAM Plugin Tool Test",
+      url: "https://mam-plugin-624886803499.us-central1.run.app/",
+      custom: {
+        info: "launched-via-deep-linking",
+      },
+    },
+  ];
+
+  const response = await DeepLinkingService.createDeepLinkingResponse(
+    token,
+    items
+  );
+  return res.send(response);
 });
 
 const setup = async () => {
   await lti.deploy({ port: process.env.PORT || 8080 });
+
   lti.app.use("/", express.static(path.join(__dirname, "public")));
-  //   console.log(path.join(__dirname, "../../frontend"));
 
   await lti.registerPlatform({
     url: process.env.PLATFORM_URL,
